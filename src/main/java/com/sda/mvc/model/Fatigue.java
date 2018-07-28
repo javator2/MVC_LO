a30228b439ecbbfa95236971b2451ac325d248cb
lukaszog <lukasz.ogan@gmail.com>
2018-07-28 12:05:04 +0200

    create mvc

---
+++ src/main/java/com/sda/mvc/model/Fatigue.java
@@ -0,0 +1,13 @@
+package com.sda.mvc.model;
+
+public enum Fatigue {
+
+    TIRED("tired"), SLEEPING("slepping");
+
+    private String title;
+
+    Fatigue(String titile){
+        this.title=titile;
+    }
+
+}
